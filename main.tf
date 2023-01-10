79fed3864d89c225c0791b99ee978b45480b7b53
Sharifoff <efqanserifov@gmail.com>
2023-01-10 11:31:30 +0400

    push

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-terraform{
-	required_version = ">=0.12"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
-	required_providers{
-		aws ={
-			source = "hashicrorp/aws"
-			version = ">= 3.24"
-		}
-	}
+  required_version = ">= 1.2.0"
 }
 
 
